060de8572fa9eabb3932b70ab53d8c5811b2aa3a
Jasmine Grozinger <114686310+Jasminegrz@users.noreply.github.com>
2024-07-01 11:39:01 +0200

    added queries to find all countries and a country by his code

--- src/resolvers/CountryResolver.ts
+++ src/resolvers/CountryResolver.ts
@@ -1,12 +1,24 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import Country from "../entities/Country";
 import { NewCountryInputType } from "../types/NewContryInputType";
+import { GraphQLError } from "graphql";
 
 @Resolver(Country)
 export default class CountryResolver {
   @Query(() => [Country])
   async countries() {
     return Country.find();
+  }
+
+  @Query(() => Country)
+  async getCountryByCode(@Arg("code") code: string) {
+    const country = await Country.findOne({
+      where: { code },
+    });
+    if (!country) {
+      throw new GraphQLError("Country not found");
+    }
+    return country;
   }
 
   @Mutation(() => Country)
